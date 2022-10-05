838ec70785e4e1fbede23e9787602b4e858713f1
Tiefu Zhu <tiefu.zhu@gmail.com>
2022-10-05 11:17:35 +1100

    finish local weather

--- frontenddev/local weather/index.js
+++ frontenddev/local weather/index.js
@@ -1,66 +1,100 @@
 function LocalWeather() {
-	const [location, setLocation] = React.useState({});
-	const [error, setError] = React.useState(null);
+    const [location, setLocation] = React.useState({ lat: '', lon: '' })
+    const [city, setCity] = React.useState('')
+    const [temp, setTemp] = React.useState(0)
+    const [weather, setWeather] = React.useState('')
+    const [humidity, setHumidity] = React.useState(0)
+    const [windSpeed, setWindSpeed] = React.useState(0)
+    const [celsius, setCelsius] = React.useState(true)
 
-	React.useEffect(() => {
-		getLocation();
-		fetchWeatherData();
-		console.log(location);
-	}, []);
+    const fCCWeatherApi = 'https://weather-proxy.freecodecamp.rocks/api/current?'
 
-	function getLocation() {
-		const success = (position) => {
-			const { latitude, longitude } = position.coords;
-			setLocation({ latitude, longitude });
-		};
-		const error = (error) => {
-			setError(error.message);
-		};
-		navigator.geolocation.getCurrentPosition(success, error);
-	}
+    const onSuccess = (position) => {
+        setLocation({
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+        })
+    }
+    const onError = (error) => {
+        console.log(error.message)
+    }
 
-	function fetchWeatherData() {
-		const { latitude, longitude } = location;
-		console.log(location);
-		const FCCapi = "https://weather-proxy.freecodecamp.rocks/api/current?";
-		fetch(
-			`${FCCapi}lat=${parseInt(latitude, 10)}&lon=${parseInt(
-				longitude,
-				10
-			)}`
-		)
-			.then((res) => res.json())
-			.then((data) => console.log(data));
-	}
+    async function fetchWeatherData() {
+        try {
+            if ('geolocation' in navigator) {
+                navigator.geolocation.getCurrentPosition(onSuccess, onError)
+            } else {
+                console.log('geolocation is not available')
+            }
+            const response = await fetch(`${fCCWeatherApi}lat=${location.lat}&lon=${location.lon}`)
+            const data = await response.json()
+            setCity(data.name)
+            setTemp(data.main.temp)
+            setWeather(data.weather[0].main)
+            setHumidity(data.main.humidity)
+            setWindSpeed(data.wind.speed)
+            // console.log(data)
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
-	return (
-		<div className="container">
-			<div className="locale h2">
-				{error ? error : "Sydney"}
-				<span>{location.latitude} </span>
-				<span>{location.longitude}</span>
-			</div>
-			<div className="weather">
-				<div className="temp h1">
-					16<span id="degree">°C</span>
-				</div>
-				<div className="weather">
-					<img src="" alt="" className="icon" />
-					<div className="description">Cloudy</div>
-				</div>
-				<div className="d-flex justify-content-between">
-					<span>Humidity: </span>
-					<span>84</span>
-				</div>
-				<div className="d-flex justify-content-between">
-					<span>Wind Speed: </span>
-					<span>1.34km/s</span>
-				</div>
-			</div>
-		</div>
-	);
+    function getWeatherIcon() {
+        switch (weather) {
+            case 'Drizzle':
+                return 'wi wi-day-sleet'
+            case 'rain':
+                return 'wi wi-day-rain'
+            case 'snow':
+                return 'wi wi-day-snow'
+            case 'clear':
+                return 'wi wi-day-sunny'
+            case 'thunderstorm':
+                return 'wi wi-day-snow-thunderstorm'
+            default:
+                return 'wi wi-day-cloudy'
+        }
+    }
+
+    function switchTempUnit() {
+        setCelsius(!celsius)
+    }
+
+    React.useEffect(() => {
+        fetchWeatherData()
+    }, [])
+
+    return (
+        <div className='container'>
+            <div className='locale h2'>
+                Weather in {city}
+                <span>{location.latitude} </span>
+                <span>{location.longitude}</span>
+            </div>
+            <div className='weather'>
+                <div className='temp'>
+                    {celsius ? temp : (parseInt(temp) * 9) / 5 + 32}
+                    <span id='degree' onClick={() => switchTempUnit()}>
+                        {celsius ? '°C' : '°F'}
+                    </span>
+                </div>
+                <div className='weatherDesc d-flex justify-content-start align-items-center'>
+                    <i className={getWeatherIcon()}></i>
+                    <div className='description'>{weather}</div>
+                </div>
+                <div className='d-flex justify-content-between'>
+                    <span>Humidity: </span>
+                    <span>{humidity}</span>
+                </div>
+                <div className='d-flex justify-content-between'>
+                    <span>Wind Speed: </span>
+                    <span>{windSpeed}km/s</span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-const container = document.getElementById("app");
-const root = ReactDOM.createRoot(container);
-root.render(<LocalWeather />);
+const container = document.getElementById('app')
+const root = ReactDOM.createRoot(container)
+root.render(<LocalWeather />)
